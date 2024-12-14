6ffde3a1a42800b9f753d33068d4974266b3e614
Mercede Chen <mercedechen@gmail.com>
2024-12-14 05:13:03 -0500

    WIP add submit button and update JS to capture value when date is selected

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-// Event listener to detect changes when a date is selected
-document.getElementById("dateInput").addEventListener("change", calculateDates);
+const dateInput = document.getElementById("dateInput");
+const submitButton = document.getElementById("submitButton");
+
+// Adds an event listener for when Submit button is clicked and captures the string value of the date selected
+submitButton.addEventListener("click", () => {
+  calculateDates();
+});
 
 function calculateDates() {
-  // Variable names for items
-  const dateInput = document.getElementById("dateInput");
-  const selectedDate = new Date(dateInput.value);
+  // Date() is a constructor that creates a Date object. When called as a function, it returns a string representing the current time.
+  const dateSelected = new Date(dateInput.value).toISOString();
+  console.log("Date Selected", dateSelected);
 
   // Days to calculate
-  const daysToAdd = [3, 7, 12, 13, 14, 15, 16-25, 26, 27, 28];
+  const daysToSubtract = [3];
 
+  //.forEach() Array method executes a provided function once for each array element.
+  daysToSubtract.forEach(day => {
+    // Clones the date selected so that each calculation starts from the unmodified state
+    const dateCopy = new Date(dateSelected).toISOString();
+    console.log("Date Copy", dateCopy);
 
+    const dateCalculated = dateCopy - day;
+    console.log("Calculated Date", dateCalculated);
+  });
 }
